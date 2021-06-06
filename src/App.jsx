ecd633f108969b54018238de578d6b8efa146af8
z0ccc <z0cccccccc@gmail.com>
2021-06-05 20:08:49 -0400

    Added hardware info

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,33 @@ const App = () => {
     document.getElementById(
       'screenSize'
     ).innerHTML += `Screen resolution: ${window.screen.width}x${window.screen.height}`;
+
+    navigator.getBattery().then((battery) => {
+      document.getElementById(
+        'batteryLevel'
+      ).innerHTML += `Battery level: ${Math.round(battery.level * 100)}%`;
+      document.getElementById(
+        'batteryStatus'
+      ).innerHTML += `Battery status: ${(battery.charging) ? '' : 'not '} charging`;
+    });
+
+    document.getElementById(
+      'memory'
+    ).innerHTML += `Device memory: ${navigator.deviceMemory}GB`;
+
+    document.getElementById(
+      'cores'
+    ).innerHTML += `# of CPU cores: ${navigator.hardwareConcurrency}`;
   }, []);
 
   return (
     <div className="App">
       <div className="title">Hardware</div>
       <div className="item" id="screenSize" />
+      <div className="item" id="batteryLevel" />
+      <div className="item" id="batteryStatus" />
+      <div className="item" id="memory" />
+      <div className="item" id="cores" />
     </div>
   );
 };
