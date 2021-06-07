e94a09586436e0234cf0e94250fe4b606403f7ec
z0ccc <z0cccccccc@gmail.com>
2021-06-07 11:55:49 -0400

    Fixed flag loading

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,23 @@ const updateDOM = (id, text) => {
 
 const App = () => {
   const [country, setCountry] = useState('US');
+  const [flag, setFlag] = useState(false);
 
   useEffect(() => {
     fetch('http://ip-api.com/json')
       .then((response) => response.json())
       .then((data) => {
         setCountry(data.countryCode);
+        setFlag(true);
         updateDOM('ipAddress', data.query);
         updateDOM('country', data.country);
         updateDOM('region', data.regionName);
         updateDOM('city', data.city);
+        updateDOM('zip', data.zip);
         updateDOM('provider', data.isp);
       });
+
+    updateDOM('os', navigator.oscpu);
 
     updateDOM('screenSize', `${window.screen.width}x${window.screen.height}`);
 
@@ -44,7 +49,7 @@ const App = () => {
           Country:{' '}
         </div>
         <div className="flagWrap">
-          <Flag code={country} size="S" />
+          {flag === true && <Flag code={country} size="S" />}
         </div>
       </div>
       <div className="item" id="region">
@@ -53,8 +58,15 @@ const App = () => {
       <div className="item" id="city">
         City:{' '}
       </div>
+      <div className="item" id="zip">
+        Zip:{' '}
+      </div>
       <div className="item" id="provider">
         Provider:{' '}
+      </div>
+      <div className="title">Software</div>
+      <div className="item" id="browser">
+        Browser:{' '}
       </div>
       <div className="title">Hardware</div>
       <div className="item" id="screenSize">
