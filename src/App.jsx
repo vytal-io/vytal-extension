1d03f40c7055b946438ed6b263b9ee907e431e7e
z0ccc <z0cccccccc@gmail.com>
2021-06-07 16:11:31 -0400

    Added more software stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,11 @@
 import * as React from 'react';
 import { useState, useEffect } from 'react';
 import Flag from 'react-flagpack';
+import * as Bowser from 'bowser';
 import './App.css';
 
 const updateDOM = (id, text) => {
   document.getElementById(id).innerHTML += text;
-};
-
-const getBrowser = (userAgent) => {
-  if (
-    (userAgent.indexOf('Opera') ||
-      userAgent.indexOf('OPR')) !== -1
-  ) {
-    return 'Opera';
-  } if (userAgent.indexOf('Edg') !== -1) {
-    return 'Edge';
-  } if (userAgent.indexOf('Chrome') !== -1) {
-    return 'Chrome';
-  } if (userAgent.indexOf('Safari') !== -1) {
-    return 'Safari';
-  } if (userAgent.indexOf('Firefox') !== -1) {
-    return 'Firefox';
-  } if (
-    userAgent.indexOf('MSIE') !== -1 ||
-    !!document.documentMode === true
-  ) {
-    return 'IE';
-  }
-  return 'unknown';
 };
 
 const App = () => {
@@ -48,9 +26,21 @@ const App = () => {
         updateDOM('provider', data.isp);
       });
 
-    updateDOM('browser', getBrowser(navigator.userAgent));
+    const uaResult = Bowser.parse(navigator.userAgent);
+    updateDOM('browser', uaResult.browser.name);
+    updateDOM('browserVersion', uaResult.browser.version);
+    updateDOM('browserEngine', uaResult.engine.name);
+    updateDOM('os', `${uaResult.os.name} ${uaResult.os.versionName}`);
 
+    updateDOM('language', navigator.language);
+    updateDOM('timezone', Intl.DateTimeFormat().resolvedOptions().timeZone);
+    updateDOM('cookies', navigator.cookieEnabled);
+    updateDOM('java', navigator.javaEnabled());
+    updateDOM('plugins', navigator.plugins);
+
+    updateDOM('platform', uaResult.platform.type);
     updateDOM('screenSize', `${window.screen.width}x${window.screen.height}`);
+    updateDOM('color', window.screen.colorDepth);
 
     navigator.getBattery().then((battery) => {
       updateDOM('batteryLevel', `${Math.round(battery.level * 100)}%`);
@@ -91,9 +81,39 @@ const App = () => {
       <div className="item" id="browser">
         Browser:{' '}
       </div>
+      <div className="item" id="browserVersion">
+        Browser version:{' '}
+      </div>
+      <div className="item" id="browserEngine">
+        Browser engine:{' '}
+      </div>
+      <div className="item" id="os">
+        Operating system:{' '}
+      </div>
+      <div className="item" id="language">
+        Language:{' '}
+      </div>
+      <div className="item" id="timezone">
+        Timezone:{' '}
+      </div>
+      <div className="item" id="cookies">
+        Cookies enabled:{' '}
+      </div>
+      <div className="item" id="java">
+        Java enabled:{' '}
+      </div>
+      <div className="item" id="plugins">
+        Plugins:{' '}
+      </div>
       <div className="title">Hardware</div>
+      <div className="item" id="platform">
+        Platform:{' '}
+      </div>
       <div className="item" id="screenSize">
         Screen resolution:{' '}
+      </div>
+      <div className="item" id="color">
+        Color Resolution:{' '}
       </div>
       <div className="item" id="batteryLevel">
         Battery level:{' '}
