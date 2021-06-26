096fb4790d3c4b222564492a6bcdd4372820de89
z0ccc <z0cccccccc@gmail.com>
2021-06-26 18:25:26 -0400

    Changed options html

--- src/components/popup.js
+++ src/components/popup.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './App';
-import '../styles/popup.css';
 
 const mountNode = document.getElementById('popup');
 ReactDOM.render(<App />, mountNode);
