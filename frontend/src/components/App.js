bb50a3ccbec5a5d7bdfc5c4696c14fea5bb333b2
z0ccc <z0cccccccc@gmail.com>
2021-07-24 01:35:30 -0400

    image link to github

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,10 +1,12 @@
 import Particles from 'react-tsparticles';
 import particlesOptions from '../particles.json';
+import Github from './Github';
 import MainColumn from './MainColumn';
 import '../styles/App.css';
 
 const App = () => (
   <div className="App">
+    <Github />
     <Particles options={particlesOptions} />
     <MainColumn />
   </div>
---
+++ frontend/src/components/Github.js
@@ -0,0 +1,9 @@
+import { ReactComponent as GithubImg } from '../images/github.svg';
+
+const Github = () => (
+  <a href="https://github.com/z0ccc/Vytal" className="github" target="_blank" rel="noreferrer">
+    <GithubImg />
+  </a>
+);
+
+export default Github;
