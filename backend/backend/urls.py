cb1a866511ca1a989f88097088f0b0b37e62f6d0
z0ccc <z0cccccccc@gmail.com>
2021-07-26 23:15:40 -0400

    Removed 'api' from url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -8,5 +8,5 @@ router.register(r'fingerprint', views.FingerprintView, 'fingerprint')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('', include(router.urls)),
 ]
