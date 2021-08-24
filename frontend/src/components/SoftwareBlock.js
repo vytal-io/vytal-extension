4353d95b5684e4c8bbeee64606779cf2ea24313d
z0ccc <z0cccccccc@gmail.com>
2021-08-24 15:58:08 -0400

    Removed useState and useEffect from software component

--- frontend/src/components/SoftwareBlock.js
+++ frontend/src/components/SoftwareBlock.js
@@ -1,25 +1,16 @@
-import { useState, useEffect } from 'react';
 import ScanBlock from './ScanBlock';
 import Table from './Table';
 import { getSoftware } from './main';
 
-const HardwareBlock = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    setData(getSoftware());
-  }, []);
-
-  return (
-    <ScanBlock>
-      <h1>Software</h1>
-      <Table data={data} />
-      <p>
-        <b>Explanation:</b> JavaScript can be used to find information about
-        your software. This information can be used to create a fingerprint.
-      </p>
-    </ScanBlock>
-  );
-};
+const HardwareBlock = () => (
+  <ScanBlock>
+    <h1>Software</h1>
+    <Table data={getSoftware()} />
+    <p>
+      <b>Explanation:</b> JavaScript can be used to find information about your
+      software. This information can be used to create a fingerprint.
+    </p>
+  </ScanBlock>
+);
 
 export default HardwareBlock;
