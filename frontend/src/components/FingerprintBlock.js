c59a75b24ee780ee0fff07a11d2556d75baba8d3
z0ccc <z0cccccccc@gmail.com>
2021-08-24 16:34:51 -0400

    Removed uneeded useEffect

--- frontend/src/components/FingerprintBlock.js
+++ frontend/src/components/FingerprintBlock.js
@@ -1,5 +1,4 @@
-/* eslint-disable no-unused-vars */
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import ScanBlock from './ScanBlock';
 import Table from './Table';
 import {
