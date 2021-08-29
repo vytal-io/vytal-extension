839d9e20f24d9ff9aaf4d98b0e988283de0a5023
z0ccc <z0cccccccc@gmail.com>
2021-08-29 14:21:29 -0400

    Added Throttle to django rest api

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -106,6 +106,14 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
     ],
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 # Internationalization
