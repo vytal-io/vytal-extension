c2633c1e50ead2fa3e740adccc32fb55d8f506af
z0ccc <z0cccccccc@gmail.com>
2021-09-21 21:10:45 -0400

    Updated meta tags

--- frontend/src/components/MainColumn.js
+++ frontend/src/components/MainColumn.js
@@ -9,7 +9,7 @@ const MainColumn = () => {
     <div className="centerBlockOuter">
       <div className="centerBlockInner">
         <Logo />
-        {scan ? <ScanBlocks /> : <StartBlock scan={scan} setScan={setScan} />}
+        {scan ? <ScanBlocks /> : <StartBlock setScan={setScan} />}
       </div>
     </div>
   );
