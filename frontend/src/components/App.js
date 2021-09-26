8f075645ddbb495f68b14b188e7f28148648c076
z0ccc <z0cccccccc@gmail.com>
2021-09-25 22:17:25 -0400

    Removed particles background

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,5 +1,3 @@
-import Particles from 'react-tsparticles';
-import particlesOptions from '../particles.json';
 import Github from './Github';
 import MainColumn from './MainColumn';
 import '../styles/App.css';
@@ -7,7 +5,7 @@ import '../styles/App.css';
 const App = () => (
   <div className="App">
     <Github />
-    <Particles options={particlesOptions} />
+    <div className="background" />
     <MainColumn />
   </div>
 );
