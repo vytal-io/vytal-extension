5cfb9c9fda1b5ccf946b51a5ff6d4af2971966e9
z0ccc <z0ccc@protonmail.com>
2022-05-07 16:15:57 -0400

    Added more profiles and changed bottom text

--- src/utils/profiles.js
+++ src/utils/profiles.js
@@ -1,10 +1,34 @@
 const profiles = {
   none: { timezone: '', locale: '', lat: '', lon: '' },
+  baghdad: {
+    timezone: 'Asia/Baghdad',
+    locale: 'ar-IQ',
+    lat: 33.3152,
+    lon: 44.3661,
+  },
+  bangkok: {
+    timezone: 'Asia/Bangkok',
+    locale: 'th-TH',
+    lat: 13.7563,
+    lon: 100.5018,
+  },
   berlin: {
     timezone: 'Europe/Berlin',
     locale: 'de-DE',
     lat: 52.520007,
     lon: 13.404954,
+  },
+  cairo: {
+    timezone: 'Africa/Cairo',
+    locale: 'ar-EG',
+    lat: 30.0444,
+    lon: 31.2357,
+  },
+  delhi: {
+    timezone: 'Asia/Kolkata',
+    locale: 'hi-IN',
+    lat: 28.7041,
+    lon: 77.1025,
   },
 }
 
